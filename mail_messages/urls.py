04cd96bdbdfdd5b9300e4cbe4d7906eaa10af6a2
Rustam Shafikov <mitaleko@gmail.com>
2025-06-20 15:16:09 +0500

    Добавление сообщений через интерфейс

--- mail_messages/urls.py
+++ mail_messages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MessageListView
+from .views import MessageListView, MessageCreateView
 
 urlpatterns = [
     path('', MessageListView.as_view(), name='message_list'),
+    path('add/', MessageCreateView.as_view(), name='message_add'),
 ]
--- mail_messages/views.py
+++ mail_messages/views.py
@@ -1,7 +1,14 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import Message
+from django.urls import reverse_lazy
 
 class MessageListView(ListView):
     model = Message
     template_name = 'mail_messages/message_list.html'
     context_object_name = 'messages'
+
+class MessageCreateView(CreateView):
+    model = Message
+    fields = ['subject', 'body']
+    template_name = 'mail_messages/message_form.html'
+    success_url = reverse_lazy('message_list')
