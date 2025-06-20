13f2c6172c38343879dddcd1d4d958db3553c74e
Rustam Shafikov <mitaleko@gmail.com>
2025-06-20 11:00:57 +0500

    Добавлена кнопка "Добавить клиента" на страницу списка

--- clients/urls.py
+++ clients/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ClientListView
+from .views import ClientListView, ClientCreateView
 
 urlpatterns = [
     path('', ClientListView.as_view(), name='client_list'),
+    path('add/', ClientCreateView.as_view(), name='client_add'),
 ]
--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,6 @@
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .models import Client
 from django.views.generic import ListView
 from .models import Client
 
@@ -5,3 +8,9 @@ class ClientListView(ListView):
     model = Client
     template_name = 'clients/client_list.html'
     context_object_name = 'clients'
+
+class ClientCreateView(CreateView):
+    model = Client
+    fields = ['email', 'full_name', 'comment']
+    template_name = 'clients/client_form.html'
+    success_url = reverse_lazy('client_list')
